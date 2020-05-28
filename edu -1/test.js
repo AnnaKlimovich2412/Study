2c39db2434d4615e8fff2cefdd5c8965c2ab65e1
AnnaKlimovich <locked_forever@mail.ru>
2020-05-28 16:57:46 +0300

    new

---
+++ edu -1/test.js
@@ -0,0 +1,43 @@
+
+var leftBorderWidht = 1;
+let second = 2;
+const pi = 3.14;
+//alert (pi);
+console.log(leftBorderWidht);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone.name);
+
+let arr = ['plum.png','orange.png','apple.png'];
+console.log(arr[2]);
+
+//alert("Hello world!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + "- object");
+let incr = 10,
+decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log(decr);
+
+let isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
